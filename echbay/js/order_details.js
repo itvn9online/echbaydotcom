43b269da7e249164dde4ee14ba3391cb273a2dd6
itvn9online <itvn9online@gmail.com>
2018-08-07 15:42:13 +0700

    thread-details-relative

--- echbay/js/order_details.js
+++ echbay/js/order_details.js
@@ -538,3 +538,69 @@ $('#eb_vandon_print').click(function () {
 
 
 
+// TEST API tạo đơn hàng tới GHN
+function WGR_GHN_get_json ( datas ) {
+	jQuery.ajax({
+		type: "POST",
+		processData: false,
+		data: JSON.stringify(datas),
+		dataType: "json",
+		contentType: "application/json; charset=utf-8",	
+//		url: "https://console.ghn.vn/api/v1/apiv3/CreateOrder"				
+		url: "//api.serverapi.host/api/v1/apiv3/CreateOrder"				
+	}).done(function( data ){
+		console.log(data);
+	});			
+}
+// https://api.ghn.vn/home/docs/detail?id=28
+if ( window.location.href.split('localhost:8888').length > 1 ) {
+	console.log('TEST GHN');
+	
+	//
+	WGR_GHN_get_json( {
+		"token": "TokenStaging",
+		"PaymentTypeID": 1,
+		"FromDistrictID": 1455,
+		"FromWardCode": "21402",
+		"ToDistrictID": 1462,
+		"ToWardCode": "21609",
+		"Note": "Tạo ĐH qua API",
+		"SealCode": "tem niêm phong",
+		"ExternalCode": "",
+		"ClientContactName": "client name",
+		"ClientContactPhone": "0987654321",
+		"ClientAddress": "140 Lê Trọng Tấn",
+		"CustomerName": "Nguyễn Văn A",
+		"CustomerPhone": "01666666666",
+		"ShippingAddress": "137 Lê Quang Định",
+		"CoDAmount": 1500000,
+		"NoteCode": "CHOXEMHANGKHONGTHU",
+		"InsuranceFee": 0,
+		"ClientHubID": 0,
+		"ServiceID": 53319,
+		"ToLatitude": 1.2343322,
+		"ToLongitude": 10.54324322,
+		"FromLat": 1.2343322,
+		"FromLng": 10.54324322,
+		"Content": "Test nội dung",
+		"CouponCode": "",
+		"Weight": 10200,
+		"Length": 10,
+		"Width": 10,
+		"Height": 10,
+		"CheckMainBankAccount": false,
+		"ShippingOrderCosts": [{
+			"ServiceID": 53337
+		}],
+		"ReturnContactName": "",
+		"ReturnContactPhone": "",
+		"ReturnAddress": "",
+		"ReturnDistrictCode": "",
+		"ExternalReturnCode": "",
+		"IsCreditCreate": true
+	} );
+}
+
+
+
+
