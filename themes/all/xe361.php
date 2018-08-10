6398e8450a825edf0a319c260b88a897f2eb75fc
itvn9online <itvn9online@gmail.com>
2018-08-10 12:39:36 +0700

    xe361

---
+++ themes/all/xe361.php
@@ -0,0 +1,32 @@
+<?php 
+/*
+* Save from Thu, 09 Nov 2017 10:19:29 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36
+*/
+$eb_all_themes_support["xe361"] = array();
+$eb_all_themes_support["xe361"]["name"] = "xe361";
+$eb_all_themes_support["xe361"]["screenshot"] = "https://img1.webgiare.org/xe361.jpg";
+$eb_all_themes_support["xe361"]["author"] = "";
+$eb_all_themes_support["xe361"]["demo"] = "";
+$eb_all_themes_support["xe361"]["price"] = 20000000;
+//
+$eb_all_themes_support["xe361"]["top"][] = "echbaytwo-top1.php";
+$eb_all_themes_support["xe361"]["top"][] = "xe361-top1.php";
+$eb_all_themes_support["xe361"]["top"][] = "top_widget.php";
+$eb_all_themes_support["xe361"]["top"][] = "bigbanner-top1.php";
+$eb_all_themes_support["xe361"]["top"][] = "breadcrumb-top1.php";
+//
+$eb_all_themes_support["xe361"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["xe361"]["footer"][] = "xe361-footer1.php";
+$eb_all_themes_support["xe361"]["footer"][] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support["xe361"]["home"][] = "xe361-home1.php";
+//$eb_all_themes_support["xe361"]["home"][] = "xe361-home2.php";
+$eb_all_themes_support["xe361"]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support["xe361"]["threaddetails"][] = "xe361-threaddetails.html";
+//
+$eb_all_themes_support["xe361"]["threadnode"][] = "xe361-threadnode.html";
