883743aef366ac0303d64781e603381482b23399
itvn9online <itvn9online@gmail.com>
2019-01-19 08:18:51 +0700

    new theme

--- themes/all/xwatchv3.php
+++ themes/all/xwatchv3.php
@@ -1,20 +1,20 @@
 <?php 
 /*
-* Save from Sun, 30 Dec 2018 07:53:28 +0700
+* Save from Fri, 18 Jan 2019 13:03:53 +0700
 * By itvn9online@yahoo.com #1
 * From IP: ::1
 * On domain: localhost:8888
 * User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36
 */
-$eb_all_themes_name = 'itsuworld';
+$eb_all_themes_name = 'xwatchv3';
 $eb_all_themes_support[$eb_all_themes_name] = array();
 $eb_all_themes_support[$eb_all_themes_name]["name"] = $eb_all_themes_name;
-$eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/itsuworld.jpg";
+$eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/xwatchv3.jpg";
 $eb_all_themes_support[$eb_all_themes_name]["author"] = "";
 $eb_all_themes_support[$eb_all_themes_name]["demo"] = "";
 $eb_all_themes_support[$eb_all_themes_name]["price"] = 4500000;
 //
-$eb_all_themes_support[$eb_all_themes_name]["top"][] = "itsuworld-top1.php";
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "xwatchv3-top1.php";
 $eb_all_themes_support[$eb_all_themes_name]["top"][] = "top_widget.php";
 // Banner tràn màn hình
 $eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
@@ -30,14 +30,14 @@ $eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2-top1.php";
 //$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2bg-top1.php";
 //
 $eb_all_themes_support[$eb_all_themes_name]["footer"][] = "footer_widget.php";
-$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "itsuworld-footer1.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "xwatchv3-footer1.php";
 // Logo đối tác
 //$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "doitac-footer1.php";
-//$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
 //
-$eb_all_themes_support[$eb_all_themes_name]["home"][] = "itsuworld-home1.php";
+$eb_all_themes_support[$eb_all_themes_name]["home"][] = "xwatchv3-home1.php";
 $eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
 //
-$eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "itsuworld-threaddetails.html";
+$eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "xwatchv3-threaddetails.html";
 //
-$eb_all_themes_support[$eb_all_themes_name]["threadnode"][] = "itsuworld-threadnode.html";
+$eb_all_themes_support[$eb_all_themes_name]["threadnode"][] = "xwatchv3-threadnode.html";
