9ff4b97040cf1dd311fca5ee549b6d10bf6dbb0e
itvn9online <itvn9online@gmail.com>
2019-05-24 08:50:58 +0700

    Update load-styles.php

--- load-styles.php
+++ load-styles.php
@@ -28,6 +28,7 @@ $dir2 = dirname( $dir ) . '/';
 //echo $dir . "\n"; print_r( $_GET ); echo $dir2 . "\n";
 
 $out = '';
+$arr_replace_url = array();
 foreach ( $load as $v ) {
 //	echo $v . "\n";
 	
@@ -35,8 +36,10 @@ foreach ( $load as $v ) {
 	if ( substr( $v, 0, 1 ) == '/' ) {
 		$out .= file_get_contents( $dir . $v, 1 );
 	}
+	// đây là các thư mục khác
 	else {
 		$out .= file_get_contents( $dir2 . $v, 1 );
+		$arr_replace_url[ dirname( $v ) ] = 1;
 	}
 }
 
@@ -44,8 +47,15 @@ foreach ( $load as $v ) {
 // thay đổi lại URL ảnh cho chuẩn xác
 $out = str_replace( '../images-global/', 'images-global/', $out );
 
+//
+//print_r( $arr_replace_url );
+foreach ( $arr_replace_url as $k => $v ) {
+	// nếu có dấu .. -> lùi lại 1 thư mục nữa
+	$out = str_replace( '../images-child/', '../' . dirname( $k ) . '/images-child/', $out );
+}
 
 echo $out;
+
 exit();
 
 
