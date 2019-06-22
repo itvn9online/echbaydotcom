18c257dfaf543c5880d089ce5b6769ed34cbeff5
itvn9online <itvn9online@gmail.com>
2019-06-22 13:13:49 +0700

    EBE_tao_bang_hoa_don_cho_echbay_wp

--- global/hoan-tat.php
+++ global/hoan-tat.php
@@ -84,11 +84,14 @@ if ( $hd_id > 0 ) {
 		$hd_mahoadon = $sql->order_sku;
 		$current_hd_object = $sql->order_products;
 		$current_tv_object = $sql->order_customer;
-//		$order_total_price = 0;
 		// tạm thời để lệnh kiểm tra đã, sau 1 tháng thì bỏ đi
-//		if ( isset( $sql->order_total_price ) ) {
+		if ( isset( $sql->order_total_price ) ) {
 			$order_total_price = $sql->order_total_price;
-//		}
+		}
+		else {
+			EBE_tao_bang_hoa_don_cho_echbay_wp();
+			$order_total_price = 0;
+		}
 		
 		
 		//
