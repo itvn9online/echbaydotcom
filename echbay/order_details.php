5e40cc6b3656b117a5b2c9faae6bfbdeddc8b122
itvn9online <itvn9online@gmail.com>
2019-06-22 13:08:00 +0700

    Update order_details.php

--- echbay/order_details.php
+++ echbay/order_details.php
@@ -291,13 +291,15 @@ else {
 			<td class="t">Giá trị đơn hàng</td>
 			<td class="i"><?php
 			// tạm thời để lệnh kiểm tra đã, sau 1 thời gian thì bỏ đi
-			if ( isset( $post->order_total_price ) ) {
+//			if ( isset( $post->order_total_price ) ) {
 				echo $post->order_total_price;
-			}
+//			}
 			// nếu chưa có -> có thể bảng hóa đơn đang thiếu -> bổ sung lại
+			/*
 			else {
 				EBE_tao_bang_hoa_don_cho_echbay_wp();
 			}
+			*/
 			?></td>
 		</tr>
 	</table>
