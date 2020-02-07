a94f5447573a8359be5cf8296b27ccad26888a4d
itvn9online <itvn9online@gmail.com>
2020-02-07 18:41:33 +0700

    Update fomo_order.js

--- javascript/fomo_order.js
+++ javascript/fomo_order.js
@@ -15,11 +15,11 @@
 		phut = Math.ceil( phut/ 60 ) - 1;
 		phut += ' phút trước';
 	}
-	else if ( phut < 24 * 3600 ) {
-		phut = '1 ngày trước';
-	}
+//	else if ( phut < 24 * 3600 ) {
+//		phut = '1 ngày trước';
+//	}
 	else {
-		phut = 'Vài ngày trước';
+		phut = 'Vài giờ trước';
 	}
 	
 	$('body').append('<ul class="fomo-order"><li class="thread-list thread-list-re"><i onclick="$(\'.fomo-order\').removeClass(\'active\')" class="fa fa-remove cur"></i><a href="' + web_link + '?p=' + a.fomo_id + '" target="_blank">&nbsp;</a><div class="cf"><div class="lf f25"><div data-size="1" class="ti-le-global fomo-avt" style="background-image: url(\'' + unescape(a.fomo_img) + '\')">&nbsp;</div></div><div class="lf f75 l19"><div class="fomo-padding"><span class="bold">' + unescape(a.fomo_name) + ' (******' + a.fomo_phone + ')</span> vừa mua<div class="bold bluecolor">' + unescape(a.fomo_product) + '</div><div class="fomo-time">' + phut + '</div></div></div></div></li></ul>');
