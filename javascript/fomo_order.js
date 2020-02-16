b966d1809eb43c60422aad64c77d8cb56ff25fea
itvn9online <itvn9online@gmail.com>
2020-02-16 07:53:11 +0700

    fomo time

--- javascript/fomo_order.js
+++ javascript/fomo_order.js
@@ -23,7 +23,7 @@
 		phut = 'Vài giờ trước';
 	}
 	
-	$('body').append('<ul class="fomo-order"><li class="thread-list thread-list-re"><i onclick="$(\'.fomo-order\').removeClass(\'active\')" class="fa fa-remove cur"></i><a href="' + web_link + '?p=' + a.fomo_id + '" target="_blank">&nbsp;</a><div class="cf"><div class="lf f25"><div data-size="1" class="ti-le-global fomo-avt" style="background-image: url(\'' + unescape(a.fomo_img) + '\')">&nbsp;</div></div><div class="lf f75 l19"><div class="fomo-padding"><span class="bold">' + unescape(a.fomo_name) + ' (******' + a.fomo_phone + ')</span> vừa mua<div class="bold bluecolor">' + unescape(a.fomo_product) + '</div><div class="fomo-time">' + phut + '</div></div></div></div></li></ul>');
+	$('body').append('<ul class="fomo-order"><li class="thread-list thread-list-re"><i onclick="$(\'.fomo-order\').removeClass(\'active\')" class="fa fa-remove cur"></i><a href="' + web_link + '?p=' + a.fomo_id + '" target="_blank">&nbsp;</a><div class="cf"><div class="lf f20"><div data-size="1" class="ti-le-global fomo-avt" style="background-image: url(\'' + unescape(a.fomo_img) + '\')">&nbsp;</div></div><div class="lf f80 l19"><div class="fomo-padding"><span class="bold">' + unescape(a.fomo_name) + ' (******' + a.fomo_phone + ')</span> vừa mua<div class="bold bluecolor">' + unescape(a.fomo_product) + '</div><div class="fomo-time">' + phut + '</div></div></div></div></li></ul>');
 	
 	setTimeout(function () {
 		/*
