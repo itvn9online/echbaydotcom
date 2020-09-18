468f32e070b2dba5517661dc926da781bbeb41d4
itvn9online <itvn9online@gmail.com>
2020-09-18 11:50:59 +0700

    Update btext.php

--- themes/all/btext.php
+++ themes/all/btext.php
@@ -1,10 +1,10 @@
 <?php 
 /*
-* Save from Fri, 14 Jun 2019 14:46:24 +0700
+* Save from Fri, 18 Sep 2020 11:50:52 +0700
 * By itvn9online@yahoo.com #1
 * From IP: ::1
 * On domain: localhost:8888
-* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36
 */
 $eb_all_themes_name = 'btext';
 $eb_all_themes_support[$eb_all_themes_name] = array();
@@ -17,7 +17,7 @@ $eb_all_themes_support[$eb_all_themes_name]["price"] = 4500000;
 $eb_all_themes_support[$eb_all_themes_name]["top"][] = "btext-top1.php";
 $eb_all_themes_support[$eb_all_themes_name]["top"][] = "top_widget.php";
 // Banner tràn màn hình
-//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
 // Banner bo gọn trong khung
 //$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbannerFixed-top1.php";
 // breadcrumb tràn màn hình
