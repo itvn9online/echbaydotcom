e73307df25946d9b0aacc76745e940ec7e648167
itvn9online <itvn9online@gmail.com>
2020-09-24 21:19:58 +0700

    news

---
+++ themes/all/news.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+* Save from Thu, 24 Aug 2017 06:47:15 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36
+*/
+$eb_all_themes_support["news"] = array();
+$eb_all_themes_support["news"]["name"] = "news";
+$eb_all_themes_support["news"]["screenshot"] = "https://img1.webgiare.org/news.jpg";
+$eb_all_themes_support["news"]["price"] = 4500000;
+//
+$eb_all_themes_support["news"]["top"][] = "news-top1.php";
+$eb_all_themes_support["news"]["top"][] = "";
+$eb_all_themes_support["news"]["top"][] = "news-top3.php";
+$eb_all_themes_support["news"]["top"][] = "echbaytwo-top4.php";
+$eb_all_themes_support["news"]["top"][] = "";
+$eb_all_themes_support["news"]["top"][] = "top_widget.php";
+//
+$eb_all_themes_support["news"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["news"]["footer"][] = "echbaytwo-footer2.php";
+$eb_all_themes_support["news"]["footer"][] = "echbaytwo-footer3.php";
+//
+//
+$eb_all_themes_support["news"]["cf_threaddetails_include_file"] = "news-threaddetails.html";
+//
+$eb_all_themes_support["news"]["cf_threadnode_include_file"] = "news-threadnode.html";
