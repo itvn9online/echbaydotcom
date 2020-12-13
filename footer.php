7729f4dedab8b6168f1f512c96ee1c4fb64d80ad
Dao Quoc Dai <itvn9online@gmail.com>
2020-12-13 18:53:00 +0700

    font awesome 5

--- footer.php
+++ footer.php
@@ -114,12 +114,16 @@ if ( eb_code_tester == true ) {
 
 ?>
 <!-- // FOOTER by <?php echo $arr_private_info_setting['author']; ?> -->
+<?php
+// nạp font awesome 5 khi có nhu cầu
+//if ( $__cf_row['cf_fontawesome_v5'] == 1 ) {
+?>
 <!-- Our project just needs Font Awesome Solid + Brands -->
-<!--
-<script src="<?php echo EB_URL_TUONG_DOI; ?>javascript/fontawesome.js"></script> 
--->
-<script defer src="<?php echo EB_URL_TUONG_DOI; ?>outsource/fontawesome-free-5.15.1-web/js/brands.min.js"></script> 
-<script defer src="<?php echo EB_URL_TUONG_DOI; ?>outsource/fontawesome-free-5.15.1-web/js/solid.min.js"></script> 
+<script defer src="<?php echo EB_URL_TUONG_DOI; ?>outsource/fontawesome-free-5.15.1-web/js/brands.min.js"></script>
+<script defer src="<?php echo EB_URL_TUONG_DOI; ?>outsource/fontawesome-free-5.15.1-web/js/solid.min.js"></script>
 <script defer src="<?php echo EB_URL_TUONG_DOI; ?>outsource/fontawesome-free-5.15.1-web/js/fontawesome.min.js"></script>
-<script defer src="<?php echo EB_URL_TUONG_DOI; ?>outsource/fontawesome-free-5.15.1-web/js/v4-shims.min.js"></script> 
+<script defer src="<?php echo EB_URL_TUONG_DOI; ?>outsource/fontawesome-free-5.15.1-web/js/v4-shims.min.js"></script>
+<?php
+//}
+?>
 </body></html>
