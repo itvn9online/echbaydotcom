9832d4a32abf27595d32d87be356611a40177881
Dao Quoc Dai <itvn9online@gmail.com>
2020-12-13 07:04:27 +0700

    font awesome admin

--- echbay/export_top.php
+++ echbay/export_top.php
@@ -10,8 +10,8 @@
 $a = array(
 //	EB_THEME_PLUGIN_INDEX . 'outsource/fa-4.7.0/i.css',
 //	EB_THEME_PLUGIN_INDEX . 'outsource/fontawesome-free-5.0.6/css/fontawesome.css',
-	//EB_THEME_PLUGIN_INDEX . 'outsource/fa-5.3.0/css/i.css',
-	//EB_THEME_PLUGIN_INDEX . 'outsource/fa-5.3.0/css/v4-shims.min.css',
+	EB_THEME_PLUGIN_INDEX . 'outsource/fa-5.3.0/css/i.css',
+	EB_THEME_PLUGIN_INDEX . 'outsource/fa-5.3.0/css/v4-shims.min.css',
 	EB_THEME_PLUGIN_INDEX . 'css/d.css',
 	EB_THEME_PLUGIN_INDEX . 'css/d2.css',
 	EB_THEME_PLUGIN_INDEX . 'css/admin.css',
