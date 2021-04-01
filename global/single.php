00e055d2a073b249bcb922deb2d0c5097b61afcc
Dao Quoc Dai <itvn9online@gmail.com>
2021-04-01 09:02:44 +0700

    Update single.php

--- global/single.php
+++ global/single.php
@@ -3,6 +3,7 @@
 //
 //echo 'aaaaaaaaaa';
 
+// có nội dung
 if ( have_posts() ) {
     //	echo '<!-- TEST -->' . "\n";
     //	print_r( $post );
@@ -22,14 +23,9 @@ if ( have_posts() ) {
         else {
             include EB_THEME_PHP . 'content.php';
         }
-    } else {
-        // hiển thị mặc định nội dung bài viết dưới dạng blog
-        echo '<!-- post_type not support: ' . $post->post_type . ' -->' . "\n";
-        //$check_post_type_supports = post_type_supports( $post->post_type, 'post-formats' );
-        //print_r( $check_post_type_supports );
-        include EB_THEME_PHP . 'content.php';
-
-        /*
+    }
+    // định dạng ads của WGR sẽ bị ẩn
+    else if ( $post->post_type == 'ads' ) {
         // sử dụng child theme
         if ( using_child_wgr_theme == 1 && file_exists( EB_CHILD_THEME_URL . 'php/404.php' ) ) {
             include EB_CHILD_THEME_URL . 'php/404.php';
@@ -38,8 +34,17 @@ if ( have_posts() ) {
         else {
             include EB_THEME_PHP . '404.php';
         }
-        */
     }
-} else {
+    // các định dạng của bên khác sẽ vẫn hỗ trợ
+    else {
+        // hiển thị mặc định nội dung bài viết dưới dạng blog
+        echo '<!-- post_type second support: ' . $post->post_type . ' -->' . "\n";
+        //$check_post_type_supports = post_type_supports( $post->post_type, 'post-formats' );
+        //print_r( $check_post_type_supports );
+        include EB_THEME_PHP . 'content.php';
+    }
+}
+// không có nội dung
+else {
     include EB_THEME_PLUGIN_INDEX . 'global/content-none.php';
 }
