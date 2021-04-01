b72a977e232333d2d62f865d6d053a3e718c22c1
Dao Quoc Dai <itvn9online@gmail.com>
2021-04-01 09:27:18 +0700

    auto redirect ads type

--- global/single.php
+++ global/single.php
@@ -26,13 +26,35 @@ if ( have_posts() ) {
     }
     // định dạng ads của WGR sẽ bị ẩn
     else if ( $post->post_type == 'ads' ) {
-        // sử dụng child theme
-        if ( using_child_wgr_theme == 1 && file_exists( EB_CHILD_THEME_URL . 'php/404.php' ) ) {
-            include EB_CHILD_THEME_URL . 'php/404.php';
+        //print_r( $post );
+        //echo $post->ID . '<br>' . "\n";
+
+        $ads_url_redirect = get_post_meta( $post->ID, '_eb_ads_url' );
+        //print_r( $ads_url_redirect );
+        if ( !empty( $ads_url_redirect ) ) {
+            $ads_url_redirect = $ads_url_redirect[ 0 ];
         }
-        // sử dụng theme mặc định
-        else {
-            include EB_THEME_PHP . '404.php';
+        if ( $ads_url_redirect == '' ) {
+            $ads_url_redirect = _eb_get_post_object( $post->ID, '_eb_ads_url' );
+        }
+        //echo $ads_url_redirect . '<br>' . "\n";
+        //echo web_link . '<br>' . "\n";
+        //die( 'gh ssf' );
+
+        // trỏ thẳng tới URL mà người dùng hướng tới
+        if ( $ads_url_redirect != '' ) {
+            wp_redirect( $ads_url_redirect );
+        } else {
+            //wp_redirect( web_link, 301 );
+
+            // sử dụng child theme
+            if ( using_child_wgr_theme == 1 && file_exists( EB_CHILD_THEME_URL . 'php/404.php' ) ) {
+                include EB_CHILD_THEME_URL . 'php/404.php';
+            }
+            // sử dụng theme mặc định
+            else {
+                include EB_THEME_PHP . '404.php';
+            }
         }
     }
     // các định dạng của bên khác sẽ vẫn hỗ trợ
