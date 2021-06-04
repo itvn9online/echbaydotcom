838fc5db6280094d684083d181fe6f6b7d0de9ca
itvn9online <itvn9online@gmail.com>
2021-06-05 05:42:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
 // không load các file trong ajax
 //if ( strstr( $_SERVER['REQUEST_URI'], '/admin-ajax.php' ) == false ) {
 //echo __FILE__ . '<br>' . "\n";
-include_once EB_THEME_PLUGIN_INDEX . 'index2.php';
+//echo EB_THEME_PLUGIN_INDEX . '<br>' . "\n";
+include_once __DIR__ . '/index2.php';
 //}
