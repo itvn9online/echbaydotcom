431578cb0c93b3fa789c5bdd18afeafdc5b3415d
itvn9online <itvn9online@gmail.com>
2021-08-31 09:28:48 +0700

    Update backup_ux_builder.php

--- echbay/backup_ux_builder.php
+++ echbay/backup_ux_builder.php
@@ -11,10 +11,10 @@ if ( defined( 'EB_CHILD_THEME_URL' ) ) {
 
     //
     if ( is_dir( $ux_builder_dir ) ) {
-        $day_backup = date( 'Ymd-H' );
+        //$day_backup = date( 'Ymd-H' );
 
         //
-        $sql = _eb_q( "SELECT ID, post_name, post_content, post_status, post_type
+        $sql = _eb_q( "SELECT *
         FROM
             `" . wp_posts . "`
         WHERE
@@ -23,12 +23,26 @@ if ( defined( 'EB_CHILD_THEME_URL' ) ) {
         //print_r( $sql );
 
         foreach ( $sql as $v ) {
+            //echo $v->post_date . '<br>' . "\n";
+            //echo $v->post_date_gmt . '<br>' . "\n";
+            //echo $v->post_modified . '<br>' . "\n";
+            //echo $v->post_modified_gmt . '<br>' . "\n";
+
+            // lấy ngày thay đổi cuối để tạo backup -> nếu không có thay đổi thì không tạo backup
+            if ( !isset( $v->post_modified_gmt ) || empty( $v->post_modified_gmt ) ) {
+                echo '$post_modified_gmt not found!: ' . $v->post_type . '#' . $v->ID . '<br>' . "\n";
+                continue;
+            }
+            $post_modified_gmt = date( 'Ymd-H', strtotime( $v->post_modified_gmt ) );
+            //echo 'post_modified_gmt: ' . $post_modified_gmt . '<br>' . "\n";
+
+            //
             if ( trim( $v->post_content ) == '' ) {
                 continue;
             }
 
             // file tồn tại rồi thì thôi, backup tầm 1 tiếng 1 lần
-            $file_backup = $ux_builder_dir . '/' . $v->post_name . '-' . $day_backup . '-' . $v->post_type . '-' . $v->ID . '.txt';
+            $file_backup = $ux_builder_dir . '/' . $v->post_name . '-' . $post_modified_gmt . '-' . $v->post_type . '-' . $v->ID . '.txt';
             if ( file_exists( $file_backup ) ) {
                 echo 'Backup exist: <em>' . basename( $file_backup ) . '</em><br>' . "\n";
                 continue;
