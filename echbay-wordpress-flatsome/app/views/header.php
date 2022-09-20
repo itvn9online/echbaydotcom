76bf525088495da7bb0b6917224b61efc360377e
itvn9online <itvn9online@gmail.com>
2022-09-20 10:21:46 +0700

    update for mobile header

--- echbay-wordpress-flatsome/app/views/header.php
+++ echbay-wordpress-flatsome/app/views/header.php
@@ -14,54 +14,60 @@ require __DIR__ . '/header_cache.php';
 <!--[if IE 9 ]> <html <?php language_attributes(); ?> class="ie9 <?php flatsome_html_classes(); ?>"> <![endif]-->
 <!--[if IE 8 ]> <html <?php language_attributes(); ?> class="ie8 <?php flatsome_html_classes(); ?>"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!-->
-<html <?php language_attributes(); ?> class="<?php flatsome_html_classes(); ?>">
+<html <?php language_attributes(); ?> class="
+<?php flatsome_html_classes(); ?>">
 <!--<![endif]-->
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!-- <base href="<?php echo web_link; ?>" /> -->
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <!-- <base href="<?php echo web_link; ?>" /> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
 
 // đặt tham số này để không nạp lại base nữa
 //define( 'HAS_BASE_HREF', true );
 
 wp_head();
 ?>
+    <style>
+    <?php echo file_get_contents(EB_THEME_PLUGIN_INDEX . 'css/mobile-usability.css', 1);
+    ?>
+    </style>
 </head>
+
 <body <?php body_class(); ?>>
-<?php do_action( 'flatsome_after_body_open' ); ?>
-<?php wp_body_open(); ?>
-<a class="skip-link screen-reader-text" href="#main">
-<?php esc_html_e( 'Skip to content', 'flatsome' ); ?>
-</a>
-<div id="wrapper">
-<?php
+    <?php do_action('flatsome_after_body_open'); ?>
+    <?php wp_body_open(); ?>
+    <a class="skip-link screen-reader-text" href="#main">
+        <?php esc_html_e('Skip to content', 'flatsome'); ?>
+    </a>
+    <div id="wrapper">
+        <?php
 
 
 /*
  * nạp top của echbaydotcom (nếu có)
  */
-if ( file_exists( EB_CHILD_THEME_URL . 'ui/' . basename( EB_CHILD_THEME_URL ) . '-top1.php' ) ) {
+if (file_exists(EB_CHILD_THEME_URL . 'ui/' . basename(EB_CHILD_THEME_URL) . '-top1.php')) {
     global $__cf_row;
     //print_r($__cf_row);
-    include EB_CHILD_THEME_URL . 'ui/' . basename( EB_CHILD_THEME_URL ) . '-top1.php';
+    include EB_CHILD_THEME_URL . 'ui/' . basename(EB_CHILD_THEME_URL) . '-top1.php';
 }
 /*
  * không thì dùng top của flatsome
  */
 else {
-    do_action( 'flatsome_before_header' );
-    ?>
-<header id="header" class="header <?php flatsome_header_classes(); ?>">
-    <div class="header-wrapper">
-        <?php get_template_part( 'template-parts/header/header', 'wrapper' ); ?>
-    </div>
-</header>
-<?php
-do_action( 'flatsome_after_header' );
+    do_action('flatsome_before_header');
+?>
+        <header id="header" class="header <?php flatsome_header_classes(); ?>">
+            <div class="header-wrapper">
+                <?php get_template_part('template-parts/header/header', 'wrapper'); ?>
+            </div>
+        </header>
+        <?php do_action('flatsome_after_header');
 }
 
 ?>
-<main id="main" class="<?php flatsome_main_classes(); ?>">
+        <main id="main" class="<?php flatsome_main_classes(); ?>">
