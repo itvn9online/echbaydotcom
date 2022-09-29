ec1d8028e4772080f0a53d9d9905056b71ee7121
itvn9online <itvn9online@gmail.com>
2022-09-29 14:57:20 +0700

    sync

--- class/widget/google_map.php
+++ class/widget/google_map.php
@@ -4,14 +4,17 @@
 /*
  * Widget bản đồ google
  */
-class ___echbay_widget_google_map extends WP_Widget {
-    function __construct() {
-        parent::__construct( 'echbay_gg_map', 'EchBay iframe (google map)', array(
+class ___echbay_widget_google_map extends WP_Widget
+{
+    function __construct()
+    {
+        parent::__construct('echbay_gg_map', 'EchBay iframe (google map)', array(
             'description' => 'Nhúng iframe vào website (thường dùng cho việc nhúng google map)'
-        ) );
+        ));
     }
 
-    function form( $instance ) {
+    function form($instance)
+    {
         $default = array(
             'title' => 'EchBay GG Map',
             'localtion' => '',
@@ -22,60 +25,62 @@ class ___echbay_widget_google_map extends WP_Widget {
             'scrolling' => 1,
             'css' => ''
         );
-        $instance = wp_parse_args( ( array )$instance, $default );
-        foreach ( $instance as $k => $v ) {
-            $$k = esc_attr( $v );
+        $instance = wp_parse_args((array)$instance, $default);
+        foreach ($instance as $k => $v) {
+            $$k = esc_attr($v);
         }
         /*
-        $title = esc_attr ( $instance ['title'] );
-        $url_video = esc_attr ( $instance ['url_video'] );
-        */
+         $title = esc_attr ( $instance ['title'] );
+         $url_video = esc_attr ( $instance ['url_video'] );
+         */
 
-        echo '<p>Title: <input type="text" class="widefat" name="' . $this->get_field_name( 'title' ) . '" value="' . $title . '" /></p>';
+        echo '<p>Title: <input type="text" class="widefat" name="' . $this->get_field_name('title') . '" value="' . $title . '" /></p>';
 
-        echo '<p class="fix-textarea-height">Địa chỉ (map): <textarea class="widefat" name="' . $this->get_field_name( 'localtion' ) . '">' . $localtion . '</textarea> <span class="small">Chỉ dùng cho google map. Khi bạn có nhiều địa chỉ khác nhau, hãy liệt kê các địa chỉ này ra đây! Mỗi địa chỉ trên một dòng.</span></p>';
+        echo '<p class="fix-textarea-height">Địa chỉ (map): <textarea class="widefat" name="' . $this->get_field_name('localtion') . '">' . $localtion . '</textarea> <span class="small">Chỉ dùng cho google map. Khi bạn có nhiều địa chỉ khác nhau, hãy liệt kê các địa chỉ này ra đây! Mỗi địa chỉ trên một dòng.</span></p>';
 
-        echo '<p>URL map hoặc iframe: <input type="text" class="widefat" name="' . $this->get_field_name( 'url_video' ) . '" onchange="return get_url_google_map_only(this);" value="' . $url_video . '" /></p>';
+        echo '<p>URL map hoặc iframe: <input type="text" class="widefat" name="' . $this->get_field_name('url_video') . '" onchange="return get_url_google_map_only(this);" value="' . $url_video . '" /></p>';
 
-        echo '<p>Chiều rộng: <input type="number" class="tiny-text" name="' . $this->get_field_name( 'width' ) . '" value="' . $width . '" /></p>';
+        echo '<p>Chiều rộng: <input type="number" class="tiny-text" name="' . $this->get_field_name('width') . '" value="' . $width . '" /></p>';
 
-        echo '<p>Chiều cao: <input type="number" class="tiny-text" name="' . $this->get_field_name( 'height' ) . '" value="' . $height . '" /></p>';
+        echo '<p>Chiều cao: <input type="number" class="tiny-text" name="' . $this->get_field_name('height') . '" value="' . $height . '" /></p>';
 
-        echo '<p>Zoom map: <input type="number" class="tiny-text" name="' . $this->get_field_name( 'zoom' ) . '" value="' . $zoom . '" /></p>';
+        echo '<p>Zoom map: <input type="number" class="tiny-text" name="' . $this->get_field_name('zoom') . '" value="' . $zoom . '" /></p>';
 
-        _eb_widget_echo_widget_input_checkbox( $this->get_field_name( 'scrolling' ), $scrolling, 'Ngăn chặn cuộn! Giúp thao tác cuộn chuột trên website không bị gián đoạn.' );
+        _eb_widget_echo_widget_input_checkbox($this->get_field_name('scrolling'), $scrolling, 'Ngăn chặn cuộn! Giúp thao tác cuộn chuột trên website không bị gián đoạn.');
 
-        echo '<p>Custom css: <input type="text" class="widefat" name="' . $this->get_field_name( 'css' ) . '" value="' . $css . '" /></p>';
+        echo '<p>Custom css: <input type="text" class="widefat" name="' . $this->get_field_name('css') . '" value="' . $css . '" /></p>';
 
         echo '<script>fix_textarea_height();</script>';
     }
 
-    function update( $new_instance, $old_instance ) {
-        $instance = _eb_widget_parse_args( $new_instance, $old_instance );
+    function update($new_instance, $old_instance)
+    {
+        $instance = _eb_widget_parse_args($new_instance, $old_instance);
         return $instance;
     }
 
-    function widget( $args, $instance ) {
+    function widget($args, $instance)
+    {
         //		global $func;
 
-        extract( $args );
+        extract($args);
 
-        $title = apply_filters( 'widget_title', $instance[ 'title' ] );
-        $localtion = isset( $instance[ 'localtion' ] ) ? trim( $instance[ 'localtion' ] ) : '';
-        $url_video = isset( $instance[ 'url_video' ] ) ? $instance[ 'url_video' ] : '';
-        $width = isset( $instance[ 'width' ] ) ? $instance[ 'width' ] : '';
-        $height = isset( $instance[ 'height' ] ) ? $instance[ 'height' ] : '';
-        $zoom = isset( $instance[ 'zoom' ] ) ? $instance[ 'zoom' ] : '';
-        $css = isset( $instance[ 'css' ] ) ? $instance[ 'css' ] : '';
+        $title = apply_filters('widget_title', $instance['title']);
+        $localtion = isset($instance['localtion']) ? trim($instance['localtion']) : '';
+        $url_video = isset($instance['url_video']) ? $instance['url_video'] : '';
+        $width = isset($instance['width']) ? $instance['width'] : '';
+        $height = isset($instance['height']) ? $instance['height'] : '';
+        $zoom = isset($instance['zoom']) ? $instance['zoom'] : '';
+        $css = isset($instance['css']) ? $instance['css'] : '';
         $scrolling = 'on';
-        if ( !isset( $instance[ 'scrolling' ] ) || $instance[ 'scrolling' ] != 'on' ) {
+        if (!isset($instance['scrolling']) || $instance['scrolling'] != 'on') {
             $scrolling = 'off';
         }
-        
+
         echo '<div class="' . $css . '">';
 
         //
-        _eb_echo_widget_name( $this->name, $before_widget );
+        _eb_echo_widget_name($this->name, $before_widget);
         //echo $before_title . ' aaaaaaaaaaaaa <br>' . "\n";
 
         //
@@ -87,16 +92,16 @@ class ___echbay_widget_google_map extends WP_Widget {
 
         //
         $aria_label = '';
-        if ( $localtion != '' ) {
-            $localtion = explode( "\n", $localtion );
+        if ($localtion != '') {
+            $localtion = explode("\n", $localtion);
             $aria_label = array();
-            foreach ( $localtion as $v ) {
-                $v = trim( $v );
-                if ( $v != '' ) {
+            foreach ($localtion as $v) {
+                $v = trim($v);
+                if ($v != '') {
                     $aria_label[] = '"' . $v . '"';
                 }
             }
-            $aria_label = str_replace( '"', '&quot;', implode( ' ', $aria_label ) );
+            $aria_label = str_replace('"', '&quot;', implode(' ', $aria_label));
         }
 
         //
