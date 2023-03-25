24c89ad448e4aff9a8b5e3873b356d10507d9b4d
itvn9online <itvn9online@gmail.com>
2023-03-26 04:30:31 +0700

    sync

--- echbay-wordpress-flatsome/autoload.php
+++ echbay-wordpress-flatsome/autoload.php
@@ -10,28 +10,30 @@ require dirname( dirname( __DIR__ ) ) . '/echbay-wordpress-flatsome/autoload.php
  * Do đoạn code viewport của flatsome chưa đạt chuẩn điểm của google page speed -> cần phải chỉnh lại
  */
 // Remove Header Viewport Meta 
-function remove_flatsome_viewport_meta() {
-    remove_action( 'wp_head', 'flatsome_viewport_meta', 1 );
+function remove_flatsome_viewport_meta()
+{
+    remove_action('wp_head', 'flatsome_viewport_meta', 1);
 }
-add_action( 'init', 'remove_flatsome_viewport_meta', 15 );
+add_action('init', 'remove_flatsome_viewport_meta', 15);
 
 
-define( 'WGR_APP_PATH', __DIR__ . '/app/' );
+define('WGR_APP_PATH', __DIR__ . '/app/');
 //define( 'WGR_PUBLIC_PATH', __DIR__ . '/public/' );
 //define( 'WGR_DEFAULT_SIDEBAR', 'main_sidebar' );
 
 /*
  * Kết nối tới theme echbaytwo
  */
-if ( !defined( 'ECHBAY_TWO_THEME' ) ) {
-    define( 'ECHBAY_TWO_THEME', dirname( EB_CHILD_THEME_URL ) . '/echbaytwo/' );
+if (!defined('ECHBAY_TWO_THEME')) {
+    define('ECHBAY_TWO_THEME', dirname(EB_CHILD_THEME_URL) . '/echbaytwo/');
 }
 //die( ECHBAY_TWO_THEME );
 require ECHBAY_TWO_THEME . 'functions.php';
 
 
 // global static class
-class Wgr {
+class Wgr
+{
     public static $eb = [];
 }
 
@@ -45,27 +47,27 @@ $arr_wgr_autoload = [
     'app/controllers',
 ];
 
-foreach ( $arr_wgr_autoload as $v ) {
-    foreach ( glob( __DIR__ . '/' . $v . '/*.php' ) as $filename ) {
+foreach ($arr_wgr_autoload as $v) {
+    foreach (glob(__DIR__ . '/' . $v . '/*.php') as $filename) {
         //echo $filename . '<br>' . "\n";
         include $filename;
 
         //
-        $classNoExt = basename( $filename, '.php' );
+        $classNoExt = basename($filename, '.php');
         //echo $classNoExt . '<br>' . "\n";
-        Wgr::$eb[ $classNoExt ] = new $classNoExt();
+        Wgr::$eb[$classNoExt] = new $classNoExt();
     }
 }
 
 // autoload library and conts file
-foreach ( glob( __DIR__ . '/app/libraries/*.php' ) as $filename ) {
+foreach (glob(__DIR__ . '/app/libraries/*.php') as $filename) {
     //echo $filename . '<br>' . "\n";
     include $filename;
 }
 
 // conver to object
 //print_r( Wgr::$eb );
-Wgr::$eb = ( object )Wgr::$eb;
+Wgr::$eb = (object)Wgr::$eb;
 //print_r( Wgr::$eb );
 
 // TEST autoload
@@ -80,12 +82,36 @@ $postModelWgr->test();
 */
 
 // nạp tất cả các file trong autoload
-foreach ( glob( WGR_APP_PATH . 'inc/autoload/*.php' ) as $filename ) {
+foreach (glob(WGR_APP_PATH . 'inc/autoload/*.php') as $filename) {
     //echo $filename . '<br>' . "\n";
     include $filename;
 }
 
-foreach ( glob( WGR_APP_PATH . 'inc/autoload/shortcode/*.php' ) as $filename ) {
+foreach (glob(WGR_APP_PATH . 'inc/autoload/shortcode/*.php') as $filename) {
     //echo $filename . '<br>' . "\n";
     include $filename;
 }
+
+//
+if (
+    $_SERVER['HTTP_HOST'] != 'webgiare.org'
+    && is_admin()
+    //&& defined('EB_CHILD_THEME_URL')
+) {
+    $flatsome_function_update = dirname(EB_CHILD_THEME_URL) . '/flatsome/inc/functions/function-update.php';
+    //echo $flatsome_function_update . PHP_EOL;
+    // nếu còn tồn tại chuỗi _site_transient_update_themes -> vẫn còn đang dùng code của flatsome
+    if (
+        file_exists($flatsome_function_update)
+        && strpos(file_get_contents($flatsome_function_update), 'webgiare_update_themes') === false
+    ) {
+        //die(__FILE__ . ':' . __LINE__);
+        echo $flatsome_function_update . PHP_EOL;
+
+        // copy file mẫu ghi đè vào file của flatsome
+        copy('https://raw.githubusercontent.com/itvn9online/webgiareorg/main/function-update.php', $flatsome_function_update);
+    }
+}
+//die(WGR_APP_PATH);
+//die(EB_CHILD_THEME_URL);
+//die(__FILE__ . ':' . __LINE__);
--- echbay/about.php
+++ echbay/about.php
@@ -7,24 +7,39 @@ global $arr_private_info_setting;
 <br>
 <div style="padding-right:10%;text-align:justify;">
     <div><a href="javascript:;" class="medium blackcolor bold admin-set-reload-url">Về tác giả</a></div>
+    <?php
+
+    //
+    if (defined('WGR_CHECKED_UPDATE_THEME')) {
+    ?>
+        <p>Phiên bản Flatsome của bạn đang được cập nhật thông qua server của webgiare.org</p>
+    <?php
+    } else {
+    ?>
+        <p>Phiên bản Flatsome của bạn đang được cập nhật thông qua server của themeforest.net</p>
+    <?php
+    }
+
+    ?>
     <p>
         <?php
-        if ( $arr_private_info_setting[ 'parent_theme_default' ] == 'echbaytwo' ) {
-            ?>
-        Giao diện được phát triển bởi <a href="http://facebook.com/ech.bay" target="_blank" rel="nofollow">Đào Quốc Đại</a>, trên nền tảng mã nguồn mở <a href="http://wordpress.org/" target="_blank" rel="nofollow">WordPress</a> và sử dụng ngôn ngữ lập trình <a href="http://php.net/" target="_blank" rel="nofollow">PHP</a>, và một số thư viện mã nguồn mở khác như <a href="http://jquery.com/" target="_blank" rel="nofollow">jQuery</a>, <a href="http://fontawesome.io/" target="_blank" rel="nofollow">Font Awesome</a>. Mọi thông tin quý vị có thể xem thêm tại website <a href="<?php echo $arr_private_info_setting['site_url']; ?>" target="_blank" rel="nofollow"><?php echo $arr_private_info_setting['site_upper']; ?></a>.<br>
-        <br>
+        if ($arr_private_info_setting['parent_theme_default'] == 'echbaytwo') {
+        ?>
+            Giao diện được phát triển bởi <a href="http://facebook.com/ech.bay" target="_blank" rel="nofollow">Đào Quốc Đại</a>, trên nền tảng mã nguồn mở <a href="http://wordpress.org/" target="_blank" rel="nofollow">WordPress</a> và sử dụng ngôn ngữ lập trình <a href="http://php.net/" target="_blank" rel="nofollow">PHP</a>, và một số thư viện mã nguồn mở khác như <a href="http://jquery.com/" target="_blank" rel="nofollow">jQuery</a>, <a href="http://fontawesome.io/" target="_blank" rel="nofollow">Font Awesome</a>. Mọi thông tin quý vị có thể xem thêm tại website <a href="<?php echo $arr_private_info_setting['site_url']; ?>" target="_blank" rel="nofollow"><?php echo $arr_private_info_setting['site_upper']; ?></a>.<br>
+            <br>
         <?php
         }
         ?>
         Để tối ưu hiệu suất của mã nguồn <a href="http://wordpress.org/" target="_blank" rel="nofollow">WordPress</a> nói chung, và giao diện được viết bởi <a href="<?php echo $arr_private_info_setting['site_url']; ?>" target="_blank" rel="nofollow"><?php echo $arr_private_info_setting['author']; ?></a> nói riêng, chúng tôi khuyến khích bạn sử dụng hosting trên hệ điều hành <a href="http://linux.com/" target="_blank" rel="nofollow"><i class="fab fa-linux"></i> Linux (CentOS)</a>, hệ thống quản trị cơ sở dữ liệu <a href="http://mariadb.com/" target="_blank" rel="nofollow">MariaDB</a> thay vì <a href="http://mysql.com/" target="_blank" rel="nofollow">MySQL</a>, webserver <a href="http://nginx.org/" target="_blank" rel="nofollow">Nginx</a> thay cho <a href="http://apache.org/" target="_blank" rel="nofollow">Apache</a>, ổ cứng chứa dữ liệu dạng SSD để cho tốc độ đọc ghi dữ liệu từ bộ nhớ đệm tốt hơn.<br>
         <br>
-        Việc sử dụng code là hoàn toàn miễn phí, bạn chỉ mất phí trong trường hợp cần tác giả hỗ trợ trực tiếp để thay đổi website theo yêu cầu. Khi đó, chúng tôi sẽ tính phí theo số giờ làm việc, mức phí có thể sẽ thay đổi vào từng thời điểm, chúng tôi sẽ báo giá cụ thể trước khi làm.</p>
+        Việc sử dụng code là hoàn toàn miễn phí, bạn chỉ mất phí trong trường hợp cần tác giả hỗ trợ trực tiếp để thay đổi website theo yêu cầu. Khi đó, chúng tôi sẽ tính phí theo số giờ làm việc, mức phí có thể sẽ thay đổi vào từng thời điểm, chúng tôi sẽ báo giá cụ thể trước khi làm.
+    </p>
     <br>
     <?php
     include ECHBAY_PRI_CODE . 'role_user.php';
 
 
-    if ( current_user_can( 'manage_options' ) ) {
+    if (current_user_can('manage_options')) {
         include ECHBAY_PRI_CODE . 'echbay_update_core.php';
         include ECHBAY_PRI_CODE . 'wordpress_update_core.php';
     }
@@ -46,30 +61,30 @@ global $arr_private_info_setting;
 /*
  * tạo thương hiệu riêng cho partner nếu không phải thương hiệu mặc định
  */
-if ( $arr_private_info_setting[ 'parent_theme_default' ] != 'echbaytwo' ) {
+if ($arr_private_info_setting['parent_theme_default'] != 'echbaytwo') {
     //echo $arr_private_info_setting[ 'parent_theme_default' ] . '<br>' . "\n";
     //echo EB_THEME_URL . '<br>' . "\n";
 
     // nếu file style vẫn tồn tại trong partner/hostingviet -> copy nội dung đó ra ngoài để thay thế
-    $dir_partner_style = EB_THEME_URL . 'partner/' . $arr_private_info_setting[ 'parent_theme_default' ];
+    $dir_partner_style = EB_THEME_URL . 'partner/' . $arr_private_info_setting['parent_theme_default'];
     //echo $dir_partner_style . '<br>' . "\n";
 
     /*
      * nếu tồn tại thư mục riêng của partner nếu tồn tại file style.css trong thư mục partner
      * cần cập nhật lại partner -> chỉ cần copy lại các file này là được
      */
-    if ( is_dir( $dir_partner_style ) && file_exists( $dir_partner_style . '/style.css' ) ) {
-        foreach ( glob( $dir_partner_style . '/*' ) as $filename ) {
+    if (is_dir($dir_partner_style) && file_exists($dir_partner_style . '/style.css')) {
+        foreach (glob($dir_partner_style . '/*') as $filename) {
             echo $filename . '<br>' . "\n";
 
-            $file_replace = EB_THEME_URL . basename( $filename );
+            $file_replace = EB_THEME_URL . basename($filename);
             echo $file_replace . '<br>' . "\n";
 
             // thay thế file
-            if ( WGR_copy( $filename, $file_replace ) ) {
-                _eb_remove_file( $filename );
+            if (WGR_copy($filename, $file_replace)) {
+                _eb_remove_file($filename);
             } else {
-                echo 'Không thay được file cho partner: ' . basename( $filename ) . '<br>' . "\n";
+                echo 'Không thay được file cho partner: ' . basename($filename) . '<br>' . "\n";
             }
         }
     }
