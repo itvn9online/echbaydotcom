19d431fd0faebc20d9fceaedc447f351a2d359c6
Dao Quoc Dai <itvn9online@gmail.com>
2023-04-03 17:46:05 +0700

    sync

--- class/custom/admin-menu.php
+++ class/custom/admin-menu.php
@@ -156,12 +156,12 @@ add_filter('admin_menu', 'echbay_create_admin_menu');
 
 
 // Tạo page để xem thông tin phiên bản
-function __wgr_version_flatsome()
+function __wgr_v2_version_flatsome()
 {
     $a = file_get_contents('https://raw.githubusercontent.com/itvn9online/webgiareorg/main/changes.txt');
     echo nl2br($a);
 }
-add_submenu_page(null, '', '', 'manage_options', 'wgr-version-flatsome', '__wgr_version_flatsome');
+add_submenu_page(null, '', '', 'manage_options', 'wgr-version-flatsome', '__wgr_v2_version_flatsome');
 
 
 //
