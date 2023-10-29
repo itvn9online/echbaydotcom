23c6394a7e269bc664902c58ead70e8e9cec11c9
itvn9online <itvn9online@gmail.com>
2023-10-29 11:37:43 +0700

    sync

--- echbay/about.php
+++ echbay/about.php
@@ -8,12 +8,12 @@ global $arr_private_info_setting;
  * Chức năng này sẽ tự động chỉnh nội dung trong file htaccess cho phù hợp
  * Để tắt chức năng này, hãy khai báo constant WGR_DISABLE_AUTO_HTACCESS trong function của child-theme
  */
-if (!defined('WGR_DISABLE_AUTO_HTACCESS')) {
-    $path_htaccess = ABSPATH . ".htaccess";
-    $content_htaccess = file_get_contents($path_htaccess);
+$path_htaccess = ABSPATH . ".htaccess";
+$content_htaccess = file_get_contents($path_htaccess);
 
-    if (strpos($content_htaccess, '{my_domain.com}') !== false) {
-        if (strpos($content_htaccess, '# Header always set Permissions-Policy ') !== false) {
+if (strpos($content_htaccess, '{my_domain.com}') !== false) {
+    if (strpos($content_htaccess, '# Header always set Permissions-Policy ') !== false) {
+        if (!defined('WGR_DISABLE_AUTO_HTACCESS')) {
             // thay thành domain hiện tại
             $content_htaccess = str_replace('{my_domain.com}', str_replace('www.', '', $_SERVER['HTTP_HOST']), $content_htaccess);
             // set header
@@ -22,8 +22,10 @@ if (!defined('WGR_DISABLE_AUTO_HTACCESS')) {
             file_put_contents($path_htaccess, $content_htaccess);
             echo 'update {my_domain.com} for ' . $path_htaccess . '<br>' . PHP_EOL;
         } else {
-            echo 'content_htaccess has {my_domain.com}' . '<br>' . PHP_EOL;
+            echo 'WGR_DISABLE_AUTO_HTACCESS is defined' . '<br>' . PHP_EOL;
         }
+    } else {
+        echo 'content_htaccess has {my_domain.com}' . '<br>' . PHP_EOL;
     }
 }
 
